4155c56462539750b7a2b02ba3b40024a0949a67
Martins Gouveia <gouveiamartins6@gmail.com>
2022-11-25 16:03:51 +0100

    ✅ Add coverage of tests

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -3,5 +3,13 @@
 describe("App", () => {
     it("open signup form", () => {
         cy.visit("/");
+
+        cy.get("input[name=name]").type("Martins Gouveia");
+        cy.get("input[name=email]").type("goy@gmail.com");
+        cy.get("input[name=password]").type("1234567890");
+
+        cy.get("input").contains("Submit").click();
+
+        cy.findByText("All right!").should("exist");
     });
 });
